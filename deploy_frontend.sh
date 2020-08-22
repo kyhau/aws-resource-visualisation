1d900493681878f00cef968e05338131e5c57170
Kay Hau <virtualda@gmail.com>
2020-08-23 09:37:03 +1000

    Updated links

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -4,8 +4,6 @@ pushd network/tangled-tree/frontend
 yarn build
 popd
 
-aws s3 sync collapsible-tree/ s3://${BUCKET_NAME}/collapsibletree/
-
 aws s3 sync network/directed-graph/ s3://${BUCKET_NAME}/network/directedgraph/
 
 aws s3 sync network/tangled-tree/frontend/build/ s3://${BUCKET_NAME}/network/tangledtree/
